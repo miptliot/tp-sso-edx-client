6fa5e15225513d84a7aac205505d23c08307e8f3
Maxim Starodubcev <maxim@trambroid.com>
2015-07-27 16:49:29 +0300

    Add multidomain cookie

---
+++ middleware.py
@@ -0,0 +1,23 @@
+from django.core.urlresolvers import reverse
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from social.apps.django_app.views import auth, NAMESPACE
+
+
+class SeamlessAuthorization(object):
+    backend = 'sso_npoed-oauth2'
+    cookie_name = 'authenticated'
+
+    def process_request(self, request):
+        auth_cookie = (request.COOKIES.get(self.cookie_name, '0').lower() in ('1', 'true', 'ok'))
+        continue_url = reverse('{0}:complete'.format(NAMESPACE), args=(self.backend,))
+        current_url = request.get_full_path()
+
+        is_continue = (continue_url in current_url)
+
+        if auth_cookie and not is_continue and not request.user.is_authenticated():
+              query_dict = request.GET.copy()
+              query_dict[REDIRECT_FIELD_NAME] = current_url
+              query_dict['auth_entry'] = 'login'
+              request.GET = query_dict
+              return auth(request, self.backend)
+        return None
---
+++ views.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.shortcuts import redirect
+
+
+def logout(request, *args, next_page=None,
+           redirect_field_name=REDIRECT_FIELD_NAME, **kwargs):
+
+    if next_page is not None:
+        next_page = resolve_url(next_page)
+
+    if (redirect_field_name in request.POST or
+            redirect_field_name in request.GET):
+        next_page = request.POST.get(redirect_field_name,
+                                     request.GET.get(redirect_field_name))
+
+    if next_page:
+        next_page = '%s%s' % (request.get_host(), next_page)
+    else:
+        next_page = request.get_host(), next_page)
+
+    return redirect('%s?%s=%s' % (settings.SOCIAL_AUTH_LOGOUT_URL,
+                                      redirect_field_name, next_page))
