3d3af1d1af63e57f511d34b267ecee2c0e3c3c24
Maxim Starodubcev <maxim@trambroid.com>
2015-07-28 11:36:22 +0300

    Add exlude url pattern

--- middleware.py
+++ middleware.py
@@ -1,5 +1,9 @@
+import re
+
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.contrib.auth import REDIRECT_FIELD_NAME, logout
+
 from social.apps.django_app.views import auth, NAMESPACE
 
 
@@ -8,19 +12,26 @@ class SeamlessAuthorization(object):
     cookie_name = 'authenticated'
 
     def process_request(self, request):
-        auth_cookie = (request.COOKIES.get(self.cookie_name, '0').lower() in ('1', 'true', 'ok'))
-        continue_url = reverse('{0}:complete'.format(NAMESPACE), args=(self.backend,))
         current_url = request.get_full_path()
+        r = re.compile(settings.SOCIAL_AUTH_EXCLUDE_URL_PATTERN)
+        if r.match(current_url):
+            return None
+
+        auth_cookie = request.COOKIES.get(self.cookie_name, '0').lower()
+        auth_cookie = (auth_cookie in ('1', 'true', 'ok'))
+        continue_url = reverse('{0}:complete'.format(NAMESPACE),
+                               args=(self.backend,))
+        is_auth = request.user.is_authenticated()
 
         is_continue = (continue_url in current_url)
 
-        if auth_cookie and not is_continue and not request.user.is_authenticated():
+        if auth_cookie and not is_continue and not is_auth:
             query_dict = request.GET.copy()
             query_dict[REDIRECT_FIELD_NAME] = current_url
             query_dict['auth_entry'] = 'login'
             request.GET = query_dict
             return auth(request, self.backend)
-        elif not auth_cookie and request.user.is_authenticated():
+        elif not auth_cookie and is_auth:
             logout(request)
 
         return None
