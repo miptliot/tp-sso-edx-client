38059e6d09c281381b82d3a36656145b5aa1b82c
Maxim Starodubcev <maxim@trambroid.com>
2015-09-03 18:10:49 +0300

    CORER-149: Hide "New library" button for not staff users

---
+++ sso_edx_npoed/utils.py
@@ -0,0 +1,28 @@
+from student.roles import (GlobalStaff, CourseStaffRole, CourseInstructorRole,
+                           CourseCreatorRole)
+from student.models import CourseAccessRole
+
+
+# Role name for OrgStaffRole, OrgInstructorRole has hardcoded in __init__ method ("staff" and "instructor")
+LIBRARY_CREATE_ROLES = [
+    'staff',
+    'instructor',
+    CourseInstructorRole.ROLE,
+    CourseStaffRole.ROLE,
+    CourseCreatorRole.ROLE
+]
+
+
+def can_create_library(user):
+    qs = CourseAccessRole.objects.filter(user_id=user.id,
+                                         role__in=LIBRARY_CREATE_ROLES)
+    is_library_creator = (
+        user.is_authenticated()
+        and user.is_active
+        and (
+            GlobalStaff().has_user(user)
+            or qs.exists()
+        )
+    )
+
+    return is_library_creator
