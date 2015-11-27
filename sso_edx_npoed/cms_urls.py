ce715eb9525d2647f5aa2b76e4840191f7862139
dorosh <dorosshh@gmai.com>
2015-11-27 10:25:32 -0500

    add cms error handlers

--- sso_edx_npoed/cms_urls.py
+++ sso_edx_npoed/cms_urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include, patterns, url
 
 
+handler404 = 'contentstore.views.render_404'
+handler500 = 'contentstore.views.render_500'
+
 urlpatterns = patterns(
     '',
+    url(r'^404$', handler404),
+    url(r'^500$', handler500),
     url(r'', include('cms.urls')),
     url(r'', include('third_party_auth.urls')),
     url(r'^social-logout', 'sso_edx_npoed.views.logout', name='social-logout'),
