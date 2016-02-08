ad014fefe907753c3eb59785cae586115e2d182d
martynovp <martynovp@gmail.com>
2016-02-08 23:23:23 +0300

    Hotfix for requests with parameters

--- sso_edx_npoed/middleware.py
+++ sso_edx_npoed/middleware.py
@@ -118,10 +118,11 @@ class PLPRedirection(object):
             return redirect(os.path.join(settings.PLP_URL, 'profile'))
 
         if start_url not in handle_local_urls or is_courses_list_or_about_page:
-            plp_url = settings.PLP_URL
-            if plp_url[-1] == '/':
-                plp_url = plp_url[:-1]
-            return redirect("%s%s" % (plp_url, current_url))
+            if start_url.split('?')[0] not in handle_local_urls:
+                plp_url = settings.PLP_URL
+                if plp_url[-1] == '/':
+                    plp_url = plp_url[:-1]
+                return redirect("%s%s" % (plp_url, current_url))
 
         is_auth = request.user.is_authenticated()
         if not is_auth and start_url not in auth_process_urls and \
