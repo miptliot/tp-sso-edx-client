9c104415dd89fe907a5ccd637712dce64e90d78b
Dmitry <dmitry.viskov@webenterprise.ru>
2019-03-22 01:45:39 +0300

    DEV-367	- Параметр "вовлеченность"

--- sso_edx_tp/middleware.py
+++ sso_edx_tp/middleware.py
@@ -137,7 +137,7 @@ class PLPRedirection(MiddlewareMixin):
             'i18n', 'search', 'verify_student', 'certificates', 'jsi18n', 'course_modes',  '404', '500','i18n.js', 'js',
             'sso', 'wiki', 'notify', 'courses', 'xblock', 'change_setting', 'account', 'notification_prefs', 'admin',
             'survey', 'event', 'instructor_task_status', 'edinsights_service', 'openassessment', 'instructor_report',
-            'media',
+            'media', 'sw.js'
         )
 
         handle_local_urls += auth_process_urls + api_urls
